fadfb32db7c117d48c7b41489f5c1a3d71e6ff2a
Arvind Singh Rawat <39412688+ArvindSinghRawat@users.noreply.github.com>
2022-12-11 23:12:42 +0530

    Added basic Open Id based security config

--- build.gradle.kts
+++ build.gradle.kts
@@ -35,7 +35,7 @@ dependencies {
 	implementation("org.springframework.boot:spring-boot-starter-actuator")
 	implementation("org.springframework.boot:spring-boot-starter-data-jpa")
 	implementation("org.springframework.boot:spring-boot-starter-data-rest")
-	implementation("org.springframework.boot:spring-boot-starter-oauth2-client")
+	implementation("org.springframework.boot:spring-boot-starter-oauth2-resource-server")
 	implementation("org.springframework.boot:spring-boot-starter-security")
 	implementation("org.springframework.boot:spring-boot-starter-web")
 	compileOnly("org.projectlombok:lombok")
