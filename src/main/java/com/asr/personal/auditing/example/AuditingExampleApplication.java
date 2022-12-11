b83b9a960c3ccdad6fa6e51bb380afa5950eaf42
Arvind Singh Rawat <39412688+ArvindSinghRawat@users.noreply.github.com>
2022-12-11 22:30:32 +0530

    Added boilerplate code

---
+++ src/main/java/com/asr/personal/auditing/example/AuditingExampleApplication.java
@@ -0,0 +1,13 @@
+package com.asr.personal.auditing.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AuditingExampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AuditingExampleApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/asr/personal/auditing/example/AuditingExampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.asr.personal.auditing.example;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AuditingExampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
